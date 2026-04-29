dca0e28ac0377bd28e045f9ba080eae0d7d70dad
Yevhen Perediela <peredelaevgen23@gmail.com>
2026-04-29 09:25:25 +0200

    first window configure

--- cryptoClicker.py
+++ cryptoClicker.py
@@ -1,1 +1,17 @@
-import tkinter
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+
+root = ctk.CTk()
+root.title("Crypto Clicker TEB")
+root.geometry("600x600")
+root.resizable(False, False)
+root.configure(fg_color="#1e1e1e")
+
+header = ctk.CTkLabel(root, text="Crypto Clicker", font=("Arial", 28, "bold"), text_color="white", fg_color="transparent")
+header.pack(pady=(20, 10))
+
+coinsLabel = ctk.CTkLabel(root, text="TebCoin balance: 0", corner_radius=15, font=("Arial", 16, "bold"), text_color="#1e1e1e", fg_color="#ffffff", width=240, height=45)
+coinsLabel.pack(pady=10)
+
+root.mainloop()
