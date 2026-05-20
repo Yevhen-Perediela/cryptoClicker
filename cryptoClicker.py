b2cbec50c1bcb22d0f85b752e17337b782271cee
Yevhen Perediela <peredelaevgen23@gmail.com>
2026-05-20 08:22:01 +0200

    main coin btn

--- cryptoClicker.py
+++ cryptoClicker.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import tkinter as tk
 
 ctk.set_appearance_mode("dark")
 
@@ -13,5 +14,31 @@ header.pack(pady=(20, 10))
 
 coinsLabel = ctk.CTkLabel(root, text="TebCoin balance: 0", corner_radius=15, font=("Arial", 16, "bold"), text_color="#1e1e1e", fg_color="#ffffff", width=240, height=45)
 coinsLabel.pack(pady=10)
+
+coin_canvas = tk.Canvas(
+    root,
+    width=270,
+    height=270,
+    bg="#1e1e1e",
+    highlightthickness=0
+)
+coin_canvas.pack(pady=25)
+
+coin_circle = coin_canvas.create_oval(
+    10, 10, 250, 250,
+    fill="#f9fe00",
+    outline=""
+)
+
+coin_text = coin_canvas.create_text(
+    130, 130,
+    text="TEBCOIN",
+    fill="black",
+    font=("Arial", 25, "bold")
+)
+
+coin_canvas.tag_bind(coin_circle, "<Button-1>", lambda event: add_coin())
+coin_canvas.tag_bind(coin_text, "<Button-1>", lambda event: add_coin())
+
 
 root.mainloop()
