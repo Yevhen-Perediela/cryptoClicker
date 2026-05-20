ebd63b62cb665a843e79b0b906891e6fc7c5ccbf
AndriiValovskyi <andriivalovskyi@gmail.com>
2026-05-20 08:28:41 +0200

    refactor: reorganize coin balance logic and add initial value

--- cryptoClicker.py
+++ cryptoClicker.py
@@ -9,11 +9,19 @@ root.geometry("600x600")
 root.resizable(False, False)
 root.configure(fg_color="#1e1e1e")
 
+
 header = ctk.CTkLabel(root, text="Crypto Clicker", font=("Arial", 28, "bold"), text_color="white", fg_color="transparent")
 header.pack(pady=(20, 10))
 
 coinsLabel = ctk.CTkLabel(root, text="TebCoin balance: 0", corner_radius=15, font=("Arial", 16, "bold"), text_color="#1e1e1e", fg_color="#ffffff", width=240, height=45)
 coinsLabel.pack(pady=10)
+
+value = 0
+
+def add_coin():
+    global value
+    value += 1
+    coinsLabel.configure(text=f"TebCoin balance: {value}")
 
 coin_canvas = tk.Canvas(
     root,
